2baa0b545e56161393d5ddaa75e1f2fd428b39de
Raghav Vishwanath <50015967+raghav-vish@users.noreply.github.com>
2020-08-01 16:47:16 +0530

    Update mf.py

--- mf.py
+++ mf.py
@@ -3,6 +3,10 @@ import bs4
 import lxml
 from openpyxl import *
 from datetime import *
+from math import *
+
+def truncate(f, n):
+    return floor(f * 10 ** n) / 10 ** n
 
 book = load_workbook('MFDetails.xlsx')
 funds = book["Fundwise Details"]
@@ -12,8 +16,9 @@ res=requests.get("http://mf6.herokuapp.com/")
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 amt = soup.find_all("div")
 for i in range(1, len(amt)+1):
-	print(amt[i-1].text)
-	funds['K'+str(i+3)]=amt[i-1].text
+
+am=truncate(float(amt[i-1].text), 2)
+funds['K'+str(i+3)]=am
 
 funds['K1'] = 'Updated on '+str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
