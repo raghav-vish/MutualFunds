3375050cb5d4e868f05f2a293029f4724c95cf9b
raghav-vish <raghav.vish@gmail.com>
2020-08-01 14:44:42 +0530

    Changed link

--- mf.py
+++ mf.py
@@ -8,13 +8,12 @@ book = load_workbook('MFDetails.xlsx')
 funds = book["Fundwise Details"]
 row_count = funds.max_row - 4
 
-
-for i in range(1, row_count):
-	url = funds.cell(row=i+3, column=7)
-	res = requests.get(url.value)
-	soup = bs4.BeautifulSoup(res.text, 'lxml')
-	amt = soup.find_all("span", class_="amt")[0].text[2:]
-	funds['K'+str(i+3)]=amt
+res=requests.get("http://mf6.herokuapp.com/")
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+amt = soup.find_all("div")
+for i in range(1, len(amt)+1):
+	print(amt[i-1].text)
+	funds['K'+str(i+3)]=amt[i-1].text
 
 funds['K1'] = 'Updated on '+str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
 
